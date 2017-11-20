84d6aca8f1a7bdd8fae6a12991ad008cbffb193e
dexofan <fadedDexofan@protonmail.com>
2017-11-20 17:14:20 +1000

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+from os import path
+
 from setuptools import setup
 
 from api2ch import api2ch
 
+here = path.abspath(path.dirname(__file__))
+
 install_requires = [
     'requests>=2.8.1'
 ]
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='dvach.api',
@@ -18,12 +24,15 @@ setup(
     py_modules=['api2ch'],
     install_requires=install_requires,
     description='Python 2ch.hk API wrapper',
+    long_description=long_description,
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: API',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Internet'
+
     ]
 )
