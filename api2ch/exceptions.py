f5cf44f500a5f909f3ed5b6e84eeedb2a0b8e1d8
dexofan <fadedDexofan@protonmail.com>
2017-11-21 18:42:15 +1000

    exceptions init

--- api2ch/exceptions.py
+++ api2ch/exceptions.py
@@ -1,2 +1,11 @@
-class ApiException(Exception):
+class ChanApiException(Exception):
     pass
+
+
+class ChanApiError(ChanApiException):
+    __slots__ = ['Error', 'Reason']
+
+    def __init__(self, chan_error_data):
+        super(ChanApiError, self).__init__()
+        self.error_data = chan_error_data
+        pass
